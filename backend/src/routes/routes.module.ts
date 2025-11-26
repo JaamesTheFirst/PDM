147371d8f1e378358599bbeb79057bcd48cc9618
Francisco <fp226766@gmail.com>
2025-11-26 21:40:21 +0000

    o module para ter todos os imports

--- backend/src/routes/routes.module.ts
+++ backend/src/routes/routes.module.ts
@@ -3,11 +3,13 @@ import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { RoutesController } from './routes.controller';
 import { OtpService } from './otp.service';
+import { RoutesService } from './routes.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [HttpModule, ConfigModule],
+  imports: [HttpModule, ConfigModule, PrismaModule],
   controllers: [RoutesController],
-  providers: [OtpService],
-  exports: [OtpService],
+  providers: [OtpService, RoutesService],
+  exports: [OtpService, RoutesService],
 })
 export class RoutesModule {}
