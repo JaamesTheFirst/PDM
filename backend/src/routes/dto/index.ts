8e82bd230ab9611a79e7ffad53635126e931c2b9
Francisco <fp226766@gmail.com>
2025-11-27 01:52:06 +0000

    meti os dtos que faltavam

--- backend/src/routes/dto/index.ts
+++ backend/src/routes/dto/index.ts
@@ -2,3 +2,5 @@ export * from './plan-itinerary.dto';
 export * from './create-route-history.dto';
 export * from './get-route-history.dto';
 export * from './update-route-status.dto';
+export * from './list-history.dto';
+export * from './save-route.dto';
